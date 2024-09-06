46887d8e5b358d9308d4e0045f7de507ad69eec0
David <david.grigorian777@gmail.com>
2024-09-06 20:21:03 +0400

    Lights

---
+++ script.js
@@ -0,0 +1,50 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__icons", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__container a", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header__container .crew h4", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+
+ScrollReveal().reveal(".about__container .about__image", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__container .about__image.image__1", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".about__content", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".cast__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".media__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+
